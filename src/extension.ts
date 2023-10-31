530a605877abf803c3ef07d9538d121d55ba0914
3biubiu <2227358943@qq.com>
2023-11-01 00:30:54 +0800

    Resolve conflict

--- src/extension.ts
+++ src/extension.ts
@@ -7,12 +7,8 @@ import { autoShift } from './autoShift';
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-<<<<<<< HEAD
 	console.log('插件激活');
 	autoShift(context)
-=======
-	console.log('Congratulations, your extension "auto-shift-input" is now active!',context);
->>>>>>> 53de8c7762cee221a3001d6f9bfe42ee50c5147a
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
