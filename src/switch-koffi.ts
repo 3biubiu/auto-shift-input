8d05aef7f0b8327fb1db335e4db621bee365fa42
3biubiu <2227358943@qq.com>
2023-11-07 19:50:15 +0800

    配置修改

--- src/switch-koffi.ts
+++ src/switch-koffi.ts
@@ -4,16 +4,11 @@
 const koffi = require('koffi/indirect');
 const user32 = koffi.load('user32.dll')
 const imm32 = koffi.load('imm32.dll')
-// const user32 = new ffi.Library("user32", {
-//     "SendMessageW": ['int32', ['long', 'int32', 'int32', 'int32']],
-//     "GetForegroundWindow": ["int32", []]
-// });
+
 const GetForegroundWindow = user32.stdcall("GetForegroundWindow", "int32", [])
 const SendMessageW = user32.stdcall("SendMessageW", 'int32', ['long', 'int32', 'int32', 'int32'])
 const ImmGetDefaultIMEWnd = imm32.stdcall("ImmGetDefaultIMEWnd", "int32", ["int32"])
-// const imm = new ffi.Library("imm32", {
-//     "ImmGetDefaultIMEWnd": ["int32", ["int32"]]
-// });
+
 /**
  * 
  * @param wParam 获取输入法状态的索引值,不同输入法可能有差异
